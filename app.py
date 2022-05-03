3171b0b802c3d443a52ef379f2cb9916f400b52e
Caleb <calebngp@gmail.com>
2022-05-03 19:48:29 -0400

    k

--- app.py
+++ app.py
@@ -1,59 +1,59 @@
 from flask import Flask, render_template, url_for #Importamos las librerias a ser usadas
 import requests
 
-hacena = Flask(__name__)  #Creabamos el objetoi hacena
+app = Flask(__name__)  #Creabamos el objetoi app
 
 
-@hacena.route("/")     #Llamabamos al metodo route y le pasamos el argumento slug o url
+@app.route("/")     #Llamabamos al metodo route y le pasamos el argumento slug o url
 def inicio(): #Creamos la funcion inicio
     return render_template('inicio.html')
                 #Retornamos la renderizacion de un documento
 
-@hacena.route("/trabajana")
+@app.route("/trabajana")
 def trabajana():
     return render_template('trabajana.html')
 
-@hacena.route("/contratana")
+@app.route("/contratana")
 def contratana():
     return render_template('contratana.html')
 
-@hacena.route("/aprendena")
+@app.route("/aprendena")
 def aprendena():
     return render_template('aprendena.html')
 
-@hacena.route("/ensenana")
+@app.route("/ensenana")
 def ensenana():
     return render_template('ensenana.html')
 
-@hacena.route("/ingresana")
+@app.route("/ingresana")
 def ingresana():
     return render_template('ingresana.html')
 
-@hacena.route("/registratena")
+@app.route("/registratena")
 def registratena():
     return render_template('registratena.html')
 
-@hacena.route("/informatena")
+@app.route("/informatena")
 def informatena():
     return render_template('informatena.html')
 
-@hacena.route("/contratana_menu")
+@app.route("/contratana_menu")
 def contratana_menu():
     return render_template('contratana_menu.html')
 
-@hacena.route("/contactana")
+@app.route("/contactana")
 def contactana():
     return render_template('contactana.html')
 
-@hacena.route("/publicana")
+@app.route("/publicana")
 def publicana():
     return render_template('publicana.html')
 
-@hacena.route("/profile_card2")
+@app.route("/profile_card2")
 def profile_card2():
     return render_template('profile_card2.html')
 
-@hacena.route("/buscana")
+@app.route("/buscana")
 def buscana():
     return render_template('buscana.html')
 
